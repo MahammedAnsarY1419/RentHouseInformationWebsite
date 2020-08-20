d68ff3d6480ccdc3aba42f9220d965263cb1e4be
MahammedAnsarY1419 <69914263+MahammedAnsarY1419@users.noreply.github.com>
2020-08-20 15:11:44 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>user display page</title>
+    <link rel="stylesheet" type="text/css" href="aboutusCss.css" />
+    <link href="https://fonts.googleapis.com/css?family=Candal&display=swap" rel="stylesheet">
+    <script src="http://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <link rel="stylesheet" href="normalize.css" />  
+</head>
+<body>
+<div id="header">     
+        <div id="logoimage">
+            <img src="homelogoimg.png"/>
+        </div>
+       
+        <ul id="navbar">
+            <li><a href="DisplayUser.php">Home</a></li>
+            <li><a href="login.php">Verify Phone Number</a></li>
+            <li><a href="addinfo.php">Post Property</a></li>
+            <li><a href="aboutus.php">About</a></li>
+        </ul>
+        <div id="heading">
+            <h3>Rent House Information Portal</h3>  
+        </div> 
+        
+        <div>
+            <h1>Developed by MAHAMMEDANSAR Y</h1>
+            <h2>Students at Dr.Ambedkar Institute of Technology, Banglore</h2>
+            <h2>To Contact : mahammedansar1419@gmail.com</h2>
+        </div>
+     
+</body>
+</html>
